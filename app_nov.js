3e7c4cf176252214d185464887cc282364a80fcd
moxuse <moxuse@gmail.com>
2020-02-02 16:31:30 +0900

    initial

---
+++ app_nov.js
@@ -0,0 +1,7 @@
+window.addEventListener("scroll", function() {
+  var scrollH = document.documentElement.scrollHeight || document.body.scrollHeight;
+});
+
+var initial = document.getElementById("bg_triangle");
+
+
